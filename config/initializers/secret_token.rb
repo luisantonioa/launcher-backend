4cdd33c5a2a8e74d8e255ba058569c17f8441a57
Luis Arias <luisantonioa@gmail.com>
2012-12-01 13:30:22 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LauncherBackend::Application.config.secret_token = '940e3991f50df2f084ab82ed19b33aa5f05712fa49e812f4449fc91ed12ce5d786909d22fed2705a73841ac5f3622eed8a94a327de4a8d6e612cd11cf2bd8112'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LauncherBackend::Application.config.session_store :cookie_store, key: '_launcher-backend_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LauncherBackend::Application.config.session_store :active_record_store
